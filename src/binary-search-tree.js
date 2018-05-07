94ff6dafd2bf8ddf39406cf79c5030e1d26e7cb6
Sara Fritz <31226863+sarafritz88@users.noreply.github.com>
2018-05-07 18:13:40 -0500

    Add files via upload

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -10,7 +10,18 @@ class BinarySearchTree {
   Make sure the rules of a binary search
   tree are being adhered to */
   insert(value) {
-
+      const node = new BinarySearchTree(value);
+      const addtoTree = (val) => {
+          if (val.value >= value) {
+              if (!val.left) return val.left = node;
+              return addtoTree(val.left);
+          }
+          if (val.value <= value) {
+              if (!val.right) return val.right = node;
+              return addtoTree(val.right);
+          }
+      };
+      return addtoTree(this);
   }
 
   /* Traverses the tree until either the
@@ -18,27 +29,56 @@ class BinarySearchTree {
   or the entire tree has been searched.
   Returns true or false accordingly */
   contains(target) {
-
+      let find = false;
+      const search = (node) => {
+          if (node.value === target) {
+              find = true;
+              return;
+          } else if (node.value > target && node.left) search(node.left);
+          else if (node.value < target && node.right) search(node.right);
+      };
+      search(this);
+      return find;
   }
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */
   getMax() {
-
+    let max = this.value;
+    let node = this;
+    while (node.right) {
+      node = node.right;
+      max = node.value;
+    }
+    return max;
   }
 
   /* Traverses the tree in a 'vertical' fashion,
   from parent to child. Executes the given callback
   on each visited tree node */
   depthFirstForEach(cb) {
-
+      const values = [];
+      const each = (node) => {
+          cb(node.value);
+          if (node.left) each(node.left);
+          if (node.right) each(node.right);
+      };
+      each(this);
   }
 
   /* Traverses the tree in a 'horizontal' fashion,
   from sibling to sibling. Executes the given callback
   on each visited tree node */
   breadthFirstForEach(cb) {
-
+      const queue = [];
+      let node;
+      queue.push(this);
+      while (queue.length) {
+          node = queue.shift();
+          cb(node.value);
+          if (node.left) queue.push(node.left);
+          if (node.right) queue.push(node.right);
+      }
   }
 }
 
--- src/linked-list.js
+++ src/linked-list.js
@@ -9,26 +9,66 @@ class LinkedList {
   of the list. The `tail` pointer
   should be updated accordingly */
   addToTail(value) {
-
+      const node = {
+          value,
+          next: null
+      };
+      if (this.head === null) {
+          this.head = node;
+          this.tail = node;
+      } else {
+          this.tail.next = node;
+          this.tail = node;
+      }
   }
+
 
   /* Remove the list's `head` value 
   The `head` pointer should be updated
   accordingly */
-  removeHead() {
-
-  }
+    removeHead() {
+        if (this.head === null) {
+            return null;
+        }
+        if (this.head === this.tail) {
+            const value = this.head.value;
+            this.head = null;
+            this.tail = null;
+            return value;
+        }
+        const value = this.head.value;
+        this.head = this.head.next;
+        return value;
+    }
 
   /* Searches the list for the given value
   Returns true or false accordingly */
   contains(value) {
-
+      if (this.head === null) {
+          return false;
+      }
+      const searchLinkedList = (node) => {
+          if (node.value === value) {
+              return true;
+          }
+          if (node.next === null) {
+              return false;
+          }
+          return searchLinkedList(node.next);
+      };
+      return searchLinkedList(this.head);
   }
 
   /* Finds and returns the maximal value
   of all the values in the list */
   getMax() {
-    
+      let store = null;
+      let currentNode = this.head;
+      while (currentNode) {
+          if (currentNode.value > store) store = currentNode.value;
+          currentNode = currentNode.next;
+      }
+      return store;
   }
 }
 
